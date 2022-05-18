425f32c49c66d47c8c365abe0a24b0f1c5c783f0
Bruno Assunção <bruno.scassuncao@gmail.com>
2022-05-17 23:44:40 -0300

    feat: add feedback description tooltip

--- src/pages/Feedback/FeedbackList/FeedbackListCard/index.tsx
+++ src/pages/Feedback/FeedbackList/FeedbackListCard/index.tsx
@@ -1,5 +1,5 @@
 import { ArrowDropDown, ArrowDropUp } from '@mui/icons-material';
-import { Paper, Stack } from '@mui/material';
+import { Paper, Stack, Tooltip } from '@mui/material';
 import dayjs from 'dayjs';
 import { useSearchParams } from 'react-router-dom';
 
@@ -8,6 +8,7 @@ import { Avatar, Typography } from '../../../../components';
 export const FeedbackListCard = ({
   amount,
   created_at,
+  description,
   type,
   user_from,
   user_to,
@@ -15,53 +16,55 @@ export const FeedbackListCard = ({
   const user: UserData = type === 'sent' ? user_to : user_from;
   const [, setSearchParams] = useSearchParams();
   return (
-    <Paper
-      sx={{ mt: 4, cursor: 'pointer' }}
-      onClick={() => setSearchParams({ user: user.id })}
-    >
-      <Stack sx={{ px: 2, pt: 5.5, pb: 3.25, position: 'relative' }}>
-        <Avatar
-          sx={{
-            height: '64px',
-            width: '64px',
-            position: 'absolute',
-            top: 0,
-            left: '50%',
-            transform: 'translate(-50%, -50%)',
-          }}
-          src={user.avatar_url}
-        />
-        <Typography
-          sx={{
-            textAlign: 'center',
-            font: 'normal normal bold 18px/20px Roboto',
-            color: '#172031',
-            mb: 2,
-          }}
-        >
-          {user.name}
-        </Typography>
-        <Typography variant="overline">Pontos de Feedback</Typography>
-        <Stack direction="row" alignItems="center">
+    <Tooltip title={description}>
+      <Paper
+        sx={{ mt: 4, cursor: 'pointer' }}
+        onClick={() => setSearchParams({ user: user.id })}
+      >
+        <Stack sx={{ px: 2, pt: 5.5, pb: 3.25, position: 'relative' }}>
+          <Avatar
+            sx={{
+              height: '64px',
+              width: '64px',
+              position: 'absolute',
+              top: 0,
+              left: '50%',
+              transform: 'translate(-50%, -50%)',
+            }}
+            src={user.avatar_url}
+          />
           <Typography
-            variant="points"
-            sx={{ fontSize: '1.875rem', lineHeight: '2.3125rem' }}
+            sx={{
+              textAlign: 'center',
+              font: 'normal normal bold 18px/20px Roboto',
+              color: '#172031',
+              mb: 2,
+            }}
           >
-            {amount}
+            {user.name}
           </Typography>
-          {type === 'recieved' ? (
-            <ArrowDropDown color="success" sx={{ fontSize: '2.3125rem' }} />
-          ) : (
-            <ArrowDropUp color="error" sx={{ fontSize: '2.3125rem' }} />
-          )}
+          <Typography variant="overline">Pontos de Feedback</Typography>
+          <Stack direction="row" alignItems="center">
+            <Typography
+              variant="points"
+              sx={{ fontSize: '1.875rem', lineHeight: '2.3125rem' }}
+            >
+              {amount}
+            </Typography>
+            {type === 'recieved' ? (
+              <ArrowDropDown color="success" sx={{ fontSize: '2.3125rem' }} />
+            ) : (
+              <ArrowDropUp color="error" sx={{ fontSize: '2.3125rem' }} />
+            )}
+          </Stack>
+          <Typography
+            variant="overline"
+            sx={{ position: 'absolute', bottom: '16px', right: '16px' }}
+          >
+            {dayjs(created_at).format('DD/MM/YYYY')}
+          </Typography>
         </Stack>
-        <Typography
-          variant="overline"
-          sx={{ position: 'absolute', bottom: '16px', right: '16px' }}
-        >
-          {dayjs(created_at).format('DD/MM/YYYY')}
-        </Typography>
-      </Stack>
-    </Paper>
+      </Paper>
+    </Tooltip>
   );
 };
